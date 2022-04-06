28b42756868ba514a874d1a6b6027c5c7c570a9f
Edvander Sperber <edvandersperber@gmail.com>
2022-04-06 20:08:59 -0300

    Create questao_01.c

---
+++ questao_01.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int potencia(int base, int expoente);
+
+int main()
+{
+    int base, expoente, resultado;
+    printf("Digite a base deseja para o calculo de potencia ");
+    scanf("%d", &base);
+    printf("\nDigite o expoente desejado ");
+    scanf("%d", &expoente);
+    resultado = potencia(base,expoente);
+    printf("%d elevado a %d = %d", base, expoente,resultado);
+
+    return 0;
+}
+
+int potencia(int base, int expoente)
+{
+    if(expoente == 0)
+    return 1;
+    else
+    return (base * potencia(base, expoente-1));
+}
