fdfe8c743982a803b5ce4731319a9eecd8bc9822
Edvander Sperber <edvandersperber@gmail.com>
2022-04-07 21:10:27 -0300

    quest 5

--- questao_05.c
+++ questao_05.c
@@ -1,7 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int expression (int repet);
+
 int main()
 {
-    
+    int n; 
+
+    printf("\nEntre com um valor 'n' com no máximo 2 algarismos: ");
+    scanf("%d", &n);
+
+    expression(n);
+
+    return 0;
+}
+
+int expression (int repeat)
+{
+    printf("\n");
+    int num = repeat;
+    if (num == 0)
+    {
+       return printf("Essa expressão é igual a 1\n\n");
+    }
+    else
+    {
+        int aCont, bCont;
+
+        for (aCont = 0 ; aCont < num ; aCont++)
+        {
+            printf("a");
+        }
+        for (bCont = 0 ; bCont < num ; bCont++)
+        {
+            printf("b");
+        }
+    }
+    printf("\n\n");
 }
